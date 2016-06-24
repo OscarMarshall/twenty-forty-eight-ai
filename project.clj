9807e8ae9e0e72092a956361d79790ff591b79c1
Oscar Marshall <oscar.lim.marshall@gmail.com>
2016-06-24 12:35:06 -0600

    Commit all changed files before switching to git flow

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[clj-webdriver "0.7.2"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [primitive-math "0.1.4"]
-                 [clj-webdriver "0.7.2"]
-                 [org.seleniumhq.selenium/selenium-java "2.47.2"]]
+                 [org.seleniumhq.selenium/selenium-java "2.53.0"]
+                 [prismatic/schema "1.1.0"]]
   :main ^:skip-aot twenty-forty-eight-ai.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/twenty_forty_eight_ai/ai.clj
+++ src/twenty_forty_eight_ai/ai.clj
@@ -1,7 +1,11 @@
 (ns twenty-forty-eight-ai.ai
   (:require [clojure.set :refer [intersection]]
             [clojure.math.numeric-tower :refer [abs expt]]
-            [twenty-forty-eight-ai.board :as board]))
+            [clojure.core.reducers :as r]
+            [twenty-forty-eight-ai.board :as board]
+            [clojure.core.async :as a]
+            [clojure.core.async.impl.protocols :as ap]
+            [schema.core :as s]))
 
 (defrecord Possibility [probability board])
 
@@ -11,10 +15,10 @@
           denominator           (* (count board-open-posns) 10)
           four-tile-probability (/ 1 denominator)
           two-tile-probability  (/ 9 denominator)]
-      (mapcat (fn [posn]
-                [(Possibility. four-tile-probability (assoc-in board posn 4))
-                 (Possibility. two-tile-probability (assoc-in board posn 2))])
-              board-open-posns))))
+      (r/mapcat (fn [posn]
+                  [(Possibility. four-tile-probability (assoc-in board posn 4))
+                   (Possibility. two-tile-probability (assoc-in board posn 2))])
+                board-open-posns))))
 
 (defn possible-dirs [board] (filter (partial board/shift board) (range 4)))
 
@@ -23,53 +27,70 @@
     (if (or (every? nil? (flatten board)) (board/game-over? board))
       [score]
       (->> (range 4)
-           (map (partial board/shift board))
-           (map (partial mapv (partial replace {0 nil})))
-           (map #(when (seq %) (->> (long-view score-fn % (dec moves))
-                                    (apply max score))))
+           (r/map (partial board/shift board))
+           (r/map (partial mapv (partial replace {0 nil})))
+           (r/map #(when (seq %) (->> (long-view score-fn % (dec moves))
+                                      (apply max score))))
+           (into [])
            (replace {nil 0})))))
 
 (defn score-trees [score-fn board moves]
   (if (or (zero? moves) (board/game-over? board))
     (score-fn board)
     (->> (range 4)
-         (map (partial possibilities board))
-         (map (partial pmap #(* (:probability %)
-                                (apply max (score-trees score-fn
-                                                        (:board %)
-                                                        (dec moves))))))
-         (map (partial apply +)))))
+         (pmap (fn [direction]
+                 (->> (possibilities board direction)
+                      (r/map #(* (:probability %)
+                                 (apply max (score-trees score-fn
+                                                         (:board %)
+                                                         (dec moves)))))
+                      (into [])
+                      (apply +)))))))
+
+(s/defn score-trees-async :- [(s/protocol ap/ReadPort)
+                              {[[s/Num]] [(s/protocol ap/ReadPort)]}]
+  [board :- [[s/Num]], moves :- s/Num]
+  (cond
+    (board/game-over? board) [(a/go 0) {}]
+    (zero? moves) (let [result (a/chan)]
+                    [result {board [result]}])
+    :else (->> (range 4)
+               (map (fn [direction]
+                      (->> (possibilities board direction)
+                           (r/map (fn [{:keys [board probability]}]
+                                    (a/go (* probability
+                                             (apply max
+                                                    (a/<! (score-trees-async board (dec moves))))))))
+                           (into [])))))))
 
 (defn monotonic-score [board]
   (if (board/game-over? board)
     0
     (->> board
-         (mapv (partial replace {nil 0}))
          (map-indexed (fn [index column]
-                        (-> (apply + column)
-                            (- (apply + (map (fn [x y]
-                                                  (if (> x y)
-                                                    (- x y)
-                                                    0))
-                                                column
-                                                (rest column))))
-                            (bit-shift-left (- (* (- 3 index) 15) 1)))))
+                        (let [column (replace {nil 0} column)]
+                          (-> (apply + column)
+                              (- (apply + (map (fn [x y]
+                                                 (if (> x y)
+                                                   (- x y)
+                                                   0))
+                                               column
+                                               (rest column))))
+                              (bit-shift-left (- (* (- 3 index) 15) 1))))))
          (apply +))))
 
 (def dir-str ["Up" "Right" "Down" "Left"])
 
 (defn pick-dir [board]
-  (let [possible (set (possible-dirs board))
-        _        (apply println "  Possible:" (map dir-str possible))
-        scores   (map max
-                      (score-trees #(long-view monotonic-score % 1) board 2)
-                      (score-trees #(vector (monotonic-score %)) board 3))
-        _        (println "  Scores:  " (map #(bit-shift-right (long %) 44)
-                                             scores))
-        pick     (->> scores
-                      (map-indexed vector)
-                      (filter (comp possible first))
-                      (apply max-key second)
-                      first)
-        _        (println "  Pick:    " (dir-str pick))]
-    pick))
+  (time (let [possible (set (possible-dirs board))
+              _        (apply println "  Possible:" (map dir-str possible))
+              scores   (score-trees #(long-view monotonic-score % 1) board 2)
+              _        (println "  Scores:  "
+                                (map #(bit-shift-right (long %) 44) scores))
+              pick     (->> scores
+                            (map-indexed vector)
+                            (filter (comp possible first))
+                            (apply max-key second)
+                            first)
+              _        (println "  Pick:    " (dir-str pick))]
+          pick)))
